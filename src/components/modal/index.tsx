39382aa61ed77c5f206da963c5377d5db080a137
hcweigand10 <henryweigand10@gmail.com>
2023-02-15 13:38:56 -0800

    modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -18,7 +18,7 @@ export default function Modal({ setShowModal, showModal }: Props) {
       </button> */}
       {showModal ? (
         <>
-          <div className="animate-reveal justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="animate-reveal justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mt-10">
             <div className="relative w-auto mt-20 mb-0 md:my-6 mx-auto md:max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
